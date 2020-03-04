b75da7acced4384dc1fb9242146825fee1a8f23e
cagatay <drosen_26@hotmail.com>
2020-03-04 23:51:44 +0300

    https://github.com/cagataysen/SnakeGame.git

---
+++ snake_game.py
@@ -0,0 +1,10 @@
+import turtle
+
+#Set up the screen
+
+window = turtle.Screen()
+window.title("Snake Game by @cagataysen")
+window.bgcolor("green")
+window.setup(width=600, height=600)
+window.tracer(0) #turns on/off animation
+
