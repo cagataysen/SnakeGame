de0ccb133718c5904d660e00fec5bbe92ebde980
cagatay <drosen_26@hotmail.com>
2020-03-05 01:00:42 +0300

    https://github.com/cagataysen/SnakeGame.git

--- snake_game.py
+++ snake_game.py
@@ -33,6 +33,9 @@ food.penup()  #no drawing when moving
 food.goto(0,100)
 
 
+segments = []
+
+
 #Functions
 
 def go_up():
@@ -83,11 +86,35 @@ window.onkeypress(go_right, "d")
 while True:
     window.update() 
 
+    #check for a collision with the food
     if head.distance(food) < 20:
         #Move the food to a random spot
         x = random.randint(-290,290)
         y = random.randint(-290, 290)
         food.goto(x, y)
+
+        #Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        #Move the end segments first in reverse order
+
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor() 
+        segments[index].goto(x, y)
+
+        #Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+
     move()
 
     time.sleep(delay)
